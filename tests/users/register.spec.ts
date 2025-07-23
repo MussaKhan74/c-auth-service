c24e64f0932f79acc82e605df52a91f3e67f8552
MussaKhan74 <MarkeLoof@gmail.com>
2025-07-23 20:57:38 +0000

    add test for persist user in db

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,26 @@
 import request from 'supertest'
 import app from '../../src/app'
+import { DataSource } from 'typeorm'
+import { User } from '../../src/entity/User'
+import { AppDataSource } from '../../src/config/data-source'
+import { truncateTable } from '../utils'
 
 describe('POST /auth/register', () => {
+    let connection: DataSource
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTable(connection)
+    })
+
+    afterAll(async () => {
+        await connection.destroy()
+    })
+
     describe('Given all fields', () => {
         it('should return the 201 status code', async () => {
             // AAA
@@ -48,15 +67,21 @@ describe('POST /auth/register', () => {
                 email: 'MarkeLoof@gmail.com',
                 password: 'secret',
             }
+
             // Act
-            const response = await request(app)
-                .post('/auth/register')
-                .send(userData)
-            // Assert application/json utf-8
-            expect(
-                (response.headers as Record<string, string>)['content-type'],
-            ).toEqual(expect.stringContaining('json'))
+            await request(app).post('/auth/register').send(userData)
+
+            // Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(1)
+            expect(users[0].firstName).toBe(userData.firstName)
+            expect(users[0].lastName).toBe(userData.lastName)
+            expect(users[0].email).toBe(userData.email)
         })
+
+        it.todo('should return an id of the created user')
     })
     describe('Fields are missing', () => {})
 })
