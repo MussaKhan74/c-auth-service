ae5e5fb326a4abb25c2d1454a54f931bd799785e
MussaKhan74 <MarkeLoof@gmail.com>
2025-07-25 21:49:23 +0500

    add test for unique email and hashed password

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -126,6 +126,51 @@ describe('POST /auth/register', () => {
             expect(users[0]).toHaveProperty('role')
             expect(users[0].role).toBe(Roles.CUSTOMER)
         })
+        it('should have hashed password in the database', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: 'Mussa',
+                lastName: 'Khan',
+                email: 'MarkeLoof@gmail.com',
+                password: 'secret',
+            }
+
+            // Act
+            await request(app).post('/auth/register').send(userData)
+
+            // Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            // console.log(users[0].password)
+            expect(users[0].password).not.toBe(userData.password)
+            expect(users[0].password).toHaveLength(60)
+            expect(users[0].password).toMatch(/^\$2b\$\d+\$/)
+        })
+        it('should return 400 status code if email already exists', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: 'Mussa',
+                lastName: 'Khan',
+                email: 'MarkeLoof@gmail.com',
+                password: 'secret',
+            }
+
+            const userRepository = connection.getRepository(User)
+            await userRepository.save({ ...userData, role: Roles.CUSTOMER })
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            const users = await userRepository.find()
+
+            // Assert
+            expect(response.status).toBe(400)
+            expect(users).toHaveLength(1)
+        })
     })
     describe('Fields are missing', () => {})
 })
